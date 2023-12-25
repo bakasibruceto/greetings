244f6b93d42c9d05a3c75ee44dbfe70cb87ed228
bakasibruceto <abla09027@gmail.com>
2023-12-25 17:53:15 +0800

    Update start.py

--- start.py
+++ start.py
@@ -8,17 +8,19 @@ import pyautogui
 import time
 import os
 
-load_dotenv()
+message = 'insert message here.'
 
+convo = [
+    #add convo id's here,
+]
+
+load_dotenv()
 email = os.getenv('EMAIL')
 password = os.getenv('PASSWORD')
 
-convo = [
-         #add convo id's here,
-        ]
-
 driver = webdriver.Edge()
 driver.get("https://www.messenger.com")
+
 email_field = WebDriverWait(driver, 10).until(
 EC.presence_of_element_located((By.ID, "email"))
 )
@@ -33,6 +35,6 @@ password_field.submit()
 for i in convo:
     driver.get('https://www.messenger.com/t/' + i)
     time.sleep(3)
-    pyautogui.write('This is an .env test.')
+    pyautogui.write(message)
     pyautogui.press('enter')
     time.sleep(3)
