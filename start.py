1714c383c8abebba0f7bb5d9bb22b8c02632a073
bakasibruceto <abla09027@gmail.com>
2023-12-25 17:38:50 +0800

    up

--- start.py
+++ start.py
@@ -1,37 +1,38 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from dotenv import load_dotenv
+import pyautogui
+import time
+import os
+
+load_dotenv()
+
+email = os.getenv('EMAIL')
+password = os.getenv('PASSWORD')
+
+convo = [
+         #add convo id's here,
+        ]
 
 driver = webdriver.Edge()
-
 driver.get("https://www.messenger.com")
-
 email_field = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID, "email"))
+EC.presence_of_element_located((By.ID, "email"))
 )
-
-
-
-
-# Type the email
-email_field.send_keys('abla09027@gmail.com')
-
-# Wait for the password input field to become visible
+time.sleep(3)
+email_field.send_keys(email)
 password_field = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.ID, "pass"))
 )
-
-# Type the password
-password_field.send_keys('pogisibruce69')
-
+password_field.send_keys(password)
 password_field.submit()
 
-
-
-
-driver.get('https://www.messenger.com/t/7127472443947705/')
-
-WebDriverWait(driver, 30).until(
-    EC.presence_of_element_located((By.ID, "some-id-on-the-next-page"))
-)
+for i in convo:
+    driver.get('https://www.messenger.com/t/' + i)
+    time.sleep(3)
+    pyautogui.write('This is an .env test.')
+    pyautogui.press('enter')
+    time.sleep(3)
